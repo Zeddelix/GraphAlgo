b2a1dcfe3a064848ae0e53bc4f7fdedc771cb3b5
Arthur <unlimitededen.arthur@gmail.com>
2021-03-18 15:29:07 +0100

    Ecriture des définitions de la classe SUMMIT (commun au deux possibilité)

--- src/SUMMIT.java
+++ src/SUMMIT.java
@@ -1,2 +1,31 @@
 public class SUMMIT {
+    private int key;
+    private static int autoCount = 0;
+    private Object info;
+
+    public SUMMIT(Object info) {
+
+    }
+
+    public SUMMIT(String s) {
+
+    }
+
+    public SUMMIT() {
+        this(null);
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public Object getInfo() {
+        return null;
+    }
+
+    public String ofString() {
+        String s;
+        return s;
+    }
+
 }
