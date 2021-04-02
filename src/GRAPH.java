b84ee7cfd7b7667bedb25302d4c3591f3ee933ea
Gaulthier <g.petitdemange94@orange.fr>
2021-04-02 14:33:57 +0200

    Correction

--- src/GRAPH.java
+++ src/GRAPH.java
@@ -34,42 +34,68 @@ public class GRAPH  {
 
     //GETTERS
     public List<SUMMIT> getSummits(){return summits;}
-    public List<BRIDGE> getBridges(SUMMIT s) {
-        List<BRIDGE> bridgeDeS = new ArrayList<>();
+    
+    public SUMMIT getSpecificSummits(int i){return summits.get(i);}
+    
+    public List<BRIDGE> getBridges(){
+    	return bridges;
+    }
+    
+    public List<BRIDGE> getSummitBridges(SUMMIT s) {
+        List<BRIDGE> bridgesOfS = new ArrayList<>();
         for(int i=0; i<bridges.size();++i) {
-            if(bridges.get(i).getFirstSummit()==s ||bridges.get(i).getSecondSummit()==s)
-                bridgeDeS.add(bridges.get(i));
+            if(bridges.get(i).getFirstSummit().equals(s) ||bridges.get(i).getSecondSummit().equals(s))
+                bridgesOfS.add(bridges.get(i));
         }
-        return bridgeDeS;
+        return bridgesOfS;
     }
 
     public int numberOfSummit() {
         return summits.size();
     }
+    
+    public boolean isOriented() {
+    	return oriented;
+    }
+    
+    public boolean isValued() {
+    	return valued;
+    }
 
     //ADD&REMOVE
     public void addSummit(SUMMIT s) {
-        boolean exist = false;
-        for (int i = 0; i < summits.size(); ++i) {
-            if (summits.get(i) == s)
-                exist = true;
-        }
-        if (!exist) {
-            summits.add(s);
-        }
+        if (!summits.contains(s)) summits.add(s);
+        else System.out.println("Ce sommet existe déjà");
     }
 
     public void removeSummit(SUMMIT s) {
-        summits.remove(s);
+    	 if (summits.contains(s)) summits.remove(s);
+    	 else System.out.println("Ce sommet n'existe pas");
+        
+    }
+    
+    public void addBridge(BRIDGE b) {
+        if (!bridges.contains(b)) bridges.add(b);
+        else System.out.println("Ce lien existe déjà");
+    }
+
+    public void removeBridge(BRIDGE b) {
+    	 if (bridges.contains(b)) bridges.remove(b);
+    	 else System.out.println("Ce lien n'existe pas");
+        
     }
 
     //METHODS
-    public int distance(SUMMIT s1, SUMMIT s2) {
+    public int distance(SUMMIT s1) {
     	return 0;
     }
 
     public int[] ListSummitRank(){
     	return null;
+    }
+    
+    public void distanceArray() {
+    	
     }
 
     public void tarjan() {
@@ -82,6 +108,14 @@ public class GRAPH  {
 
     public void Kruskal() {
 
+    }
+    
+    public void Dantzig() {
+
+    }
+    
+    public void Prufer() {
+    	
     }
 
     //OUTPUT
